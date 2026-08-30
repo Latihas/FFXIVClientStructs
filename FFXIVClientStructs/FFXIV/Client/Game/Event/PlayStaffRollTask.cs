b231c20cf83c004b4f92828e6a1302bd30c9d957
Latihas <lyjp2002@163.com>
2026-08-31 02:10:01 +0800

    u

--- FFXIVClientStructs/FFXIV/Client/Game/Event/PlayStaffRollTask.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/PlayStaffRollTask.cs
@@ -9,7 +9,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 [StructLayout(LayoutKind.Explicit, Size = 0x20)]
 public partial struct PlayStaffRollTask {
     [FieldOffset(0x10)] public uint StaffRollType; // not sure what id this is, but it also controls which BGM is used (hardcoded)
-    [FieldOffset(0x14)] public CreditMode Mode;
+    [FieldOffset(0x14)] public int Mode;
     [FieldOffset(0x18)] public byte StaffRollFlags;
     [FieldOffset(0x1C)] public float StartWaitTime; // maybe for the music to kick in?
 }
